78747751afb038855a0cd8881b3f1015aece3915
Jugal Bhatt <55304795+jugalbhatt123@users.noreply.github.com>
2020-04-08 21:04:45 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+var mongoose = require('mongoose')
+
+// DataBase Config
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true }
+  )
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err));
+
+const app = express()
+const port = process.env.PORT||3000
+
+
+
+app.use(express.json())
+app.listen(port,()=>{
+    console.log('Server is up on port'+port)
+})
